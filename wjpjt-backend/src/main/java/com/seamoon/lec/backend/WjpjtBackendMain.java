db5769f7d55693abd303cf0e578aa36ca01c42bc
seamoon <seamoon.dev@gmail.com>
2025-04-14 00:11:15 +0900

    feat: spring boot 메인 클래스 및 설정 파일
    - spring boot 메인 클래스 생성
    - 섦정파일 생성 - port 설정 추가

---
+++ wjpjt-backend/src/main/java/com/seamoon/lec/backend/WjpjtBackendMain.java
@@ -0,0 +1,11 @@
+package com.seamoon.lec.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjpjtBackendMain {
+    public static void main(String[] args) {
+        SpringApplication.run(WjpjtBackendMain.class, args);
+    }
+}
---
+++ wjpjt-frontend/src/main/java/com/seamoon/lec/WjpjtFrontendMain.java
@@ -0,0 +1,11 @@
+package com.seamoon.lec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjpjtFrontendMain {
+    public static void main(String[] args) {
+        SpringApplication.run(WjpjtFrontendMain.class, args);
+    }
+}
