7fe03be30319e354def8e9521b3a0074434ac36a
seamoon <seamoon.dev@gmail.com>
2025-04-13 18:54:13 +0900

    feat: 프로젝트 기본 생성
    프로젝트 기본 생성

---
+++ wjpjt-frontend/src/main/java/com/seamoon/lec/App.java
@@ -0,0 +1,13 @@
+package com.seamoon.lec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
